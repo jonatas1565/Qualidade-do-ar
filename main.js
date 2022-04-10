a99689d2d2dee33213c072a173378d83de8f07bf
jonatas1565 <jonatasferreira330@gmail.com>
2022-04-09 23:05:59 -0300

    projeto concluido

--- main.js
+++ main.js
@@ -1,57 +1,8 @@
+window.sr = ScrollReveal({reset: true})
+sr.reveal('#titulo',  {duration: 1300});
+sr.reveal('#vento',  {duration: 11020});
+sr.reveal('.card',  {
+    rotate: {x: 100, y:100, z:0},
+    duration: 3800,
 
-// const YOUR_API_KEY = "bd7c2b51-85c9-4406-b7f7-76bfe8292790"
-
-
-// var config = {
-//   method: 'get',
-//   url: 'http://api.airvisual.com/v2/nearest_city?key=bd7c2b51-85c9-4406-b7f7-76bfe8292790',
-//   headers: { }
-// };
-
-// axios(config)
-// .then(function (response) {
-//   console.log(JSON.stringify(response.data));
-// })
-// .catch(function (error) {
-//   console.log(error);
-// });
-
-// var element =  document.getElementById("resultado");
-// element.innerHTML=' <p> sua cidade:  </p>'
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// navigator.geolocation.getCurrentPosition(success);
-
-// function success(position) {
-//     // console.log(position);
-//    sendLocationApi(position.coords.latitude, position.coords.longitude);
-// }
-// async function sendLocationApi(LATITUDE, LONGITUDE) {
-//     let response = await fetch('http://api.airvisual.com/v2/nearest_city?lat=${LATITUDE}&lon=${LONGITUDE}&key=${YOUR_API_KEY}');
-
-//     let responseJson = await response.json();
-
-//     insertDataMain(responseJson.data);
-// }
-
-
-// bd7c2b51-85c9-4406-b7f7-76bfe8292790
+});
